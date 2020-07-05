18aeb5d0a72016d99c5383b165f40f284587833a
Travis Fox <travis.fox90@gmail.com>
2020-07-05 05:15:27 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,44 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/^[aeiouAEIOU]/)
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un[a-z]+ing/)
 end
 
-def words_five_letters_long(text)
+=begin
+why didn't the following work?
 
+text.scan(/^un[a-z]+ing$/)
+
+=end
+
+
+def words_five_letters_long(text_string)
+  text_string.scan(/\w{5}/)
 end
+
+
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z].*[\.\?\!]$/)
+    true
+  else
+    false
+  end
 end
 
-def valid_phone_number?(phone)
 
+
+
+def valid_phone_number?(phone)
+  if phone.match(/[\W]{1}?[\d]{3}[\W]{1}?[\d]{3}[\W]{1}?[\d]{4}[\W]{1}?/)
+    true
+  else
+    false
+  end
 end
